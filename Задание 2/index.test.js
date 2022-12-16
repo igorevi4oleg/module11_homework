90a1a8a758d95a9c727212af7b392f07f8915f93
igorevi4oleg <105513065+igorevi4oleg@users.noreply.github.com>
2022-12-16 12:10:49 +0300

    Update index.test.js

--- Задание 2/index.test.js
+++ Задание 2/index.test.js
@@ -3,4 +3,6 @@ import { getMonth } from "./index";
 describe("tests for check month name", () => {
 
     it("should be not a number", () => expect(getMonth('aSAsdsa')).toBe('Необходимо указать число'));
+    it("should be incorrect input", () => expect(getMonth(13)).toBe('Число указано некорректно'));
+    it("should be month name", () => expect(getMonth(12)).toBe('декабрь'));
 });
